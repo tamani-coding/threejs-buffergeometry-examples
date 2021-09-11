46dc0b05f946e1af2453e9236f3bcc1c6626f8cc
genka <tamani.coding@gmail.com>
2021-09-11 14:53:07 +0200

    sphere with waves

--- src/sphere_with_waves.ts
+++ src/sphere_with_waves.ts
@@ -31,15 +31,15 @@ export function sphere_with_waves() {
     dirLight.castShadow = true
     dirLight.shadow.mapSize.width = 4096;
     dirLight.shadow.mapSize.height = 4096;
-    const d = 25;
+    const d = 10;
     dirLight.shadow.camera.left = - d;
     dirLight.shadow.camera.right = d;
     dirLight.shadow.camera.top = d;
     dirLight.shadow.camera.bottom = - d;
-    dirLight.position.z = -30;
+    dirLight.position.z = -25;
 
     let target = new THREE.Object3D();
-    target.position.z = -20;
+    target.position.z = -30;
     dirLight.target = target;
     dirLight.target.updateMatrixWorld();
 
@@ -58,7 +58,7 @@ export function sphere_with_waves() {
     const waterAmbientOcclusion = textureLoader.load("./textures/water/Water_002_OCC.jpg");
 
     // PLANE
-    const geometry = new THREE.SphereBufferGeometry(6, 64, 64);
+    const geometry = new THREE.SphereBufferGeometry(6, 150, 150);
     const sphere = new THREE.Mesh(geometry,
         new THREE.MeshStandardMaterial({
             map: waterBaseColor,
@@ -69,7 +69,7 @@ export function sphere_with_waves() {
         }));
     sphere.receiveShadow = true;
     sphere.castShadow = true;
-    sphere.rotation.x = - Math.PI / 2;
+    sphere.rotation.x = - Math.PI / 4;
     sphere.position.z = - 30;
     scene.add(sphere);
 
@@ -81,32 +81,67 @@ export function sphere_with_waves() {
     const normals_clone = JSON.parse(JSON.stringify(normals)) as Float32Array;
     const uvs = (geometry.attributes.uv.array as Float32Array);
     const damping = 0.2;
+    const vector3 = new THREE.Vector3();
+    const quaternion = new THREE.Quaternion();
 
     // ANIMATE
     function animate() {
-
-        // WAVES
         const now = Date.now() / 200;
+
+        // iterate all vertices
         for (let i = 0; i < count; i++) {
+            // indices
             const ix = i * 3
             const iy = i * 3 + 1
             const iz = i * 3 + 2
 
+            // normal vector
             const nX = normals_clone[ix];
             const nY = normals_clone[iy];
             const nZ = normals_clone[iz];
 
+            // use uvs to calculate wave
             const uX = uvs[i * 2] * Math.PI * 16
             const uY = uvs[i * 2 + 1]  * Math.PI * 16
 
+            // calculate wave height
             const xangle = (uX + now)
             const xsin = Math.sin(xangle) * damping
             const yangle = (uY + now)
             const ycos = Math.cos(yangle) * damping
 
+            // apply wave height using original position & original normal vector
             position[ix] = position_clone[ix] + nX * (xsin + ycos);
             position[iy] = position_clone[iy] + nY * (xsin + ycos);
             position[iz] = position_clone[iz] + nZ * (xsin + ycos);
+
+            // calculate normal vector of wave slopes
+            const tsx = 1 / Math.sqrt(1 + Math.pow(Math.cos(xangle) * damping, 2))
+            const tsy = Math.cos(xangle) * damping / Math.sqrt(1 + Math.pow(Math.cos(xangle) * damping, 2))
+            const tcx = 1 / Math.sqrt(1 + Math.pow(Math.sin(yangle) * damping, 2))
+            const tcy = Math.sin(yangle) * damping / Math.sqrt(1 + Math.pow(Math.sin(yangle) * damping, 2))
+
+            // store normal vector in 3d vector 
+            vector3.x = tsx
+            vector3.y = 0
+            vector3.z = -tsy
+            vector3.y = tcx
+            vector3.z += -tcy
+            vector3.normalize()
+
+            // create quarternion from wave normal vector
+            quaternion.setFromAxisAngle(vector3, Math.PI * 2)
+
+            // apply quarternion on original nromal vector
+            vector3.x = normals_clone[ix]
+            vector3.y = normals_clone[iy]
+            vector3.z = normals_clone[iz]
+            vector3.applyQuaternion(quaternion)
+
+            // set new normal vector
+            normals[ix] = vector3.x
+            normals[iy] = vector3.y
+            normals[iz] = vector3.z
         }
         geometry.attributes.position.needsUpdate = true;
         geometry.attributes.normal.needsUpdate = true;
